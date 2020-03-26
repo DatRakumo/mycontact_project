02ea837553e6c8ce4aebda708f87416538416421
DatRakumo <datnv@rakumo.vn>
2020-03-27 05:30:45 +0700

    [27_3_2020]fourth commit

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,5 @@
 # appengine_config.py
-import pkg_resources
+
 from google.appengine.ext import vendor
 
 # Set path to your libraries folder.
@@ -7,4 +7,3 @@ path = 'lib'
 # Add libraries installed in the path folder.
 vendor.add(path)
 # Add libraries to pkg_resources working set to find the distribution.
-pkg_resources.working_set.add_entry(path)
